d2a54465dcdc95b24d3a3d53e633947584a51686
tahsin314 <adarswapnik3.14@gmail.com>
2020-06-17 20:26:33 +0000

    albumentation version of hair added

--- augmentations/hair.py
+++ augmentations/hair.py
@@ -1,5 +1,10 @@
 # Courtesy: https://www.kaggle.com/nroman/melanoma-pytorch-starter-efficientnet
-# Needed to be converted to albumentations format
+# Albumentation version is also added
+import random
+import numpy as np
+import cv2 
+from albumentations.core.transforms_interface import DualTransform, ImageOnlyTransform
+
 class DrawHair:
     """
     Draw a random number of pseudo hairs
@@ -38,3 +43,36 @@ class DrawHair:
 
     def __repr__(self):
         return f'{self.__class__.__name__}(hairs={self.hairs}, width={self.width})'
+
+class Hair(ImageOnlyTransform):
+    def __init__(
+            self, hairs:int = 4, width:tuple = (1, 2),
+            always_apply=False,
+            p=0.5,
+    ):
+        super(Hair, self).__init__(always_apply, p)
+        self.hairs = hairs
+        self.width = width
+
+    def apply(self, image, **params):
+        """
+        Args:
+            img (PIL Image): Image to draw hairs on.
+
+        Returns:
+            PIL Image: Image with drawn hairs.
+        """
+        if not self.hairs:
+            return image
+        
+        width, height, _ = image.shape
+        self.image = image
+        for _ in range(random.randint(0, self.hairs)):
+            # The origin point of the line will always be at the top half of the image
+            origin = (random.randint(0, width), random.randint(0, height // 2))
+            # The end of the line 
+            end = (random.randint(0, width), random.randint(0, height))
+            color = (0, 0, 0)  # color of the hair. Black.
+            cv2.line(self.image, origin, end, color, random.randint(self.width[0], self.width[1]))
+        
+        return self.image
