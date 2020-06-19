f399c31e4ad60e2a47cee10730840d872c80c24a
tahsin314 <adarswapnik3.14@gmail.com>
2020-06-19 15:58:01 +0000

    advanced hair and microscope added

--- augmentations/hair.py
+++ augmentations/hair.py
@@ -76,3 +76,85 @@ class Hair(ImageOnlyTransform):
             cv2.line(self.image, origin, end, color, random.randint(self.width[0], self.width[1]))
         
         return self.image
+
+class AdvancedHairAugmentation:
+    '''
+    Copied from https://www.kaggle.com/c/siim-isic-melanoma-classification/discussion/159176
+    '''
+    def __init__(self, hairs: int = 4, hairs_folder: str = ""):
+        self.hairs = hairs
+        self.hairs_folder = hairs_folder
+
+    def __call__(self, img):
+        n_hairs = random.randint(0, self.hairs)
+
+        if not n_hairs:
+            return img
+
+        height, width, _ = img.shape  # target image width and height
+        hair_images = [im for im in os.listdir(self.hairs_folder) if 'png' in im]
+
+        for _ in range(n_hairs):
+            hair = cv2.imread(os.path.join(self.hairs_folder, random.choice(hair_images)))
+            hair = cv2.flip(hair, random.choice([-1, 0, 1]))
+            hair = cv2.rotate(hair, random.choice([0, 1, 2]))
+
+            h_height, h_width, _ = hair.shape  # hair image width and height
+            roi_ho = random.randint(0, img.shape[0] - hair.shape[0])
+            roi_wo = random.randint(0, img.shape[1] - hair.shape[1])
+            roi = img[roi_ho:roi_ho + h_height, roi_wo:roi_wo + h_width]
+
+            img2gray = cv2.cvtColor(hair, cv2.COLOR_BGR2GRAY)
+            ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+            mask_inv = cv2.bitwise_not(mask)
+            img_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+            hair_fg = cv2.bitwise_and(hair, hair, mask=mask)
+
+            dst = cv2.add(img_bg, hair_fg)
+            img[roi_ho:roi_ho + h_height, roi_wo:roi_wo + h_width] = dst
+
+        return img
+
+class AdvancedHairAugmentationAlbumentations(ImageOnlyTransform):
+    def __init__(self, hairs: int = 4, hairs_folder: str = "", always_apply=False,
+            p=0.5):
+        super(AdvancedHairAugmentationAlbumentations, self).__init__(always_apply, p)
+        self.hairs = hairs
+        self.hairs_folder = hairs_folder
+
+    def apply(self, image, **params):
+        """
+        Args:
+            img (PIL Image): Image to draw hairs on.
+
+        Returns:
+            PIL Image: Image with drawn hairs.
+        """
+        n_hairs = random.randint(0, self.hairs)
+
+        if not n_hairs:
+            return img
+
+        height, width, _ = img.shape  # target image width and height
+        hair_images = [im for im in os.listdir(self.hairs_folder) if 'png' in im]
+
+        for _ in range(n_hairs):
+            hair = cv2.imread(os.path.join(self.hairs_folder, random.choice(hair_images)))
+            hair = cv2.flip(hair, random.choice([-1, 0, 1]))
+            hair = cv2.rotate(hair, random.choice([0, 1, 2]))
+
+            h_height, h_width, _ = hair.shape  # hair image width and height
+            roi_ho = random.randint(0, img.shape[0] - hair.shape[0])
+            roi_wo = random.randint(0, img.shape[1] - hair.shape[1])
+            roi = img[roi_ho:roi_ho + h_height, roi_wo:roi_wo + h_width]
+
+            img2gray = cv2.cvtColor(hair, cv2.COLOR_BGR2GRAY)
+            ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+            mask_inv = cv2.bitwise_not(mask)
+            img_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+            hair_fg = cv2.bitwise_and(hair, hair, mask=mask)
+
+            dst = cv2.add(img_bg, hair_fg)
+            img[roi_ho:roi_ho + h_height, roi_wo:roi_wo + h_width] = dst
+
+        return img
